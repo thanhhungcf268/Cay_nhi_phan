9ca368ec564c8a2a3ac556efd6a61ff54af47bc6
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-04-28 13:55:13 +0700

    [Optional] [Bài tập] Tìm số nguyên tố

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        LazyPrimeFactorization lp = new LazyPrimeFactorization();
+        lp.run();
+        OptimizedPrimeFactorization op = new OptimizedPrimeFactorization();
+        op.run();
+    }
+}
---
+++ OptimizedPrimeFactorization.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class OptimizedPrimeFactorization implements Runnable {
+    Scanner scanner = new Scanner(System.in);
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+        System.out.print("Nhập n = ");
+        int n = 10;
+        System.out.printf("%d số nguyên tố đầu tiên là: \n", n);
+        int dem = 0; // đếm số số nguyên tố
+        int i = 2;   // tìm số nguyên tố bắt dầu từ số 2
+        while (dem < n) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+                dem++;
+            }
+            i++;
+        }
+        long stop = System.currentTimeMillis();
+        System.out.println((stop-start));
+    }
+    public  boolean isPrimeNumber(int n) {
+        // so nguyen n < 2 khong phai la so nguyen to
+        if (n < 2) {
+            return false;
+        }
+        // check so nguyen to khi n >= 2
+        for (int i = n-1; i>=2; i--) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
